88495dae9c0ee67d21f46fb53624587494e21a40
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-04-27 03:14:53 -0300

    feat: day9 in way

--- day9/challenges/first/first.go
+++ day9/challenges/first/first.go
@@ -8,16 +8,14 @@ import (
 )
 
 func Run() {
-	// TODO: TRANSFORMAR O RETORNO DE REPORT E UM [][]int
-	// CRIAR AS CAMADAS ADICIONAIS EM CADA INDEX DO ARRAY DE ACORDO COM O ENUNCIADO
-	// ESSA CAMADA SÓ É ADICIONADA QUANDO TODOS OS VALORES DO ULTIMO ARRAY SAO 0
 	report := getReport()
 	for i := range report {
-		extrapolateSequences(i, report)
+		extrapolateSequences(i, &report)
 	}
+	fmt.Println(sumReportsExtrapolation(report))
 }
 
-func getReport() [][]string {
+func getReport() [][]int {
 	report, err := os.ReadFile("input.txt")
 	if err != nil {
 		panic(err)
@@ -31,55 +29,97 @@ func getReport() [][]string {
 		splitedReports = append(splitedReports, strings.Split(r, " "))
 	}
 
-	return splitedReports
+	var intReports [][]int
+
+	for _, report := range splitedReports {
+		var intReport []int
+		for _, r := range report {
+			intr, err := strconv.Atoi(r)
+			if err != nil {
+				panic(err)
+			}
+			intReport = append(intReport, intr)
+		}
+		intReports = append(intReports, intReport)
+	}
+
+	return intReports
 }
 
-func extrapolateSequences(index int, report [][]string) {
-	sequence := report[index]
-	var extrapolates [][]string
-	extrapolates = append(extrapolates, sequence)
-	for i := 0; i < len(extrapolates); i++ {
-		newSequence, exit := createSequences(extrapolates, i, len(extrapolates[i])-1)
-		extrapolates = append(extrapolates, newSequence)
+func extrapolateSequences(index int, report *[][]int) {
+	sequence := (*report)[index]
 
+	fmt.Printf("Before Report \n%v\n\n", (*report)[index])
+	var sequences [][]int
+	sequences = append(sequences, sequence)
+	for i := 0; i < len(sequences); i++ {
+		exit := createSequences(&sequences, i, len(sequences[i])-1)
 		if exit {
+			extrapolate(&sequences)
 			break
 		}
 	}
-	fmt.Println(extrapolates)
+
+	for _, v := range sequences {
+		(*report)[index] = v
+		break
+	}
+	fmt.Printf("New Sequence \n\n")
+	fmt.Printf("Report \n%v\n\n", (*report)[index])
+	fmt.Println("Sequences")
+	for _, r := range sequences {
+		fmt.Println(r)
+	}
+	fmt.Println("")
+	fmt.Println("")
 }
 
-func createSequences(extrapolates [][]string, currentIndex, newSequenceLen int) ([]string, bool) {
-	currentSequence := extrapolates[currentIndex]
-	var newSequence []string
+func createSequences(sequences *[][]int, currentIndex, newSequenceLen int) bool {
+	currentSequence := (*sequences)[currentIndex]
+	var newSequence []int
 
 	for i := 0; i < newSequenceLen; i++ {
-		currSeqValue, err := strconv.Atoi(currentSequence[i])
-		if err != nil {
-			panic(err)
-		}
-		currSeqNextValue, err := strconv.Atoi(currentSequence[i+1])
-		if err != nil {
-			panic(err)
-		}
+		currSeqValue := currentSequence[i]
+		currSeqNextValue := currentSequence[i+1]
 		currSeqValueDiff := currSeqValue - currSeqNextValue
 
 		if currSeqValueDiff < 0 {
 			currSeqValueDiff *= -1
 		}
-		newSequence = append(newSequence, fmt.Sprintf("%d", currSeqValueDiff))
+		newSequence = append(newSequence, currSeqValueDiff)
 	}
 
-	if canExit(newSequence) {
-		return newSequence, true
-	}
+	*sequences = append((*sequences), newSequence)
 
-	return newSequence, false
+	return canExit(newSequence)
 }
 
-func canExit(sequence []string) bool {
+func extrapolate(sequences *[][]int) {
+	for i := len(*sequences) - 1; i >= 0; i-- {
+		if i == len(*sequences)-1 {
+			(*sequences)[i] = append((*sequences)[i], 0)
+			continue
+		}
+		beforeSeq := (*sequences)[i+1]
+		beforeSeqLastValue := beforeSeq[len(beforeSeq)-1]
+		currSeq := (*sequences)[i]
+		currSeqLastValue := currSeq[len(currSeq)-1]
+		// fmt.Printf("last value: %d\n before last value: %d\n", currSeqLastValue, beforeSeqLastValue)
+		(*sequences)[i] = append((*sequences)[i], beforeSeqLastValue+currSeqLastValue)
+	}
+}
+
+func sumReportsExtrapolation(report [][]int) int {
+	sum := 0
+	for _, r := range report {
+		sum += r[len(r)-1]
+	}
+	return sum
+}
+
+func canExit(sequence []int) bool {
 	for _, v := range sequence {
-		if v != "0" {
+		if v != 0 {
 			return false
 		}
 	}
