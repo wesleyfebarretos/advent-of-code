2404375568a6c384965a3ba870a292354b230783
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-04-28 22:53:53 -0300

    feat: day9

--- day9/challenges/second/second.go
+++ day9/challenges/second/second.go
@@ -1,3 +1,114 @@
 package challenge2
 
-func Run() {}
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Run() {
+	report := getReport()
+	for i := range report {
+		extrapolateSequences(i, &report)
+	}
+	fmt.Println(sumReportsExtrapolation(report))
+}
+
+func getReport() [][]int {
+	report, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	reports := strings.Split(strings.TrimSpace(string(report)), "\n")
+
+	var splitedReports [][]string
+
+	for _, r := range reports {
+		splitedReports = append(splitedReports, strings.Split(r, " "))
+	}
+
+	var intReports [][]int
+
+	for _, report := range splitedReports {
+		var intReport []int
+		for _, r := range report {
+			intr, err := strconv.Atoi(r)
+			if err != nil {
+				panic(err)
+			}
+			intReport = append(intReport, intr)
+		}
+		intReports = append(intReports, intReport)
+	}
+
+	return intReports
+}
+
+func extrapolateSequences(index int, report *[][]int) {
+	sequence := (*report)[index]
+
+	var sequences [][]int
+	sequences = append(sequences, sequence)
+	for i := 0; i < len(sequences); i++ {
+		exit := createSequences(&sequences, i, len(sequences[i])-1)
+		if exit {
+			extrapolate(&sequences)
+			break
+		}
+	}
+
+	for _, v := range sequences {
+		(*report)[index] = v
+		break
+	}
+}
+
+func createSequences(sequences *[][]int, currentIndex, newSequenceLen int) bool {
+	currentSequence := (*sequences)[currentIndex]
+	var newSequence []int
+
+	for i := 0; i < newSequenceLen; i++ {
+		currSeqValue := currentSequence[i]
+		currSeqNextValue := currentSequence[i+1]
+		currSeqValueDiff := currSeqNextValue - currSeqValue
+
+		newSequence = append(newSequence, currSeqValueDiff)
+	}
+
+	*sequences = append((*sequences), newSequence)
+
+	return canExit(newSequence)
+}
+
+func extrapolate(sequences *[][]int) {
+	for i := len(*sequences) - 1; i >= 0; i-- {
+		if i == len(*sequences)-1 {
+			(*sequences)[i] = append([]int{0}, (*sequences)[i]...)
+			continue
+		}
+		beforeSeq := (*sequences)[i+1]
+		beforeSeqFirstValue := beforeSeq[0]
+		currSeq := (*sequences)[i]
+		currSeqFirstValue := currSeq[0]
+		(*sequences)[i] = append([]int{currSeqFirstValue - beforeSeqFirstValue}, (*sequences)[i]...)
+	}
+}
+
+func sumReportsExtrapolation(report [][]int) int {
+	sum := 0
+	for _, r := range report {
+		sum += r[0]
+	}
+	return sum
+}
+
+func canExit(sequence []int) bool {
+	for _, v := range sequence {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
--- day9/main.go
+++ day9/main.go
@@ -1,7 +1,16 @@
 package main
 
-import challenge1 "github.com/wesleyfebarretos/aoc/day9/challenges/first"
+import (
+	"fmt"
+
+	challenge1 "github.com/wesleyfebarretos/aoc/day9/challenges/first"
+	challenge2 "github.com/wesleyfebarretos/aoc/day9/challenges/second"
+)
 
 func main() {
+	fmt.Print("First challenge: ")
 	challenge1.Run()
+
+	fmt.Print("Second challenge: ")
+	challenge2.Run()
 }
