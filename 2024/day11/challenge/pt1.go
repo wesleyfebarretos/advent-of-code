dd78438718ee7bcc3c2b2d3b5f0c5c2270e1797d
Wesley Ferreira <wesleyfebarretos@gmail.com>
2024-12-23 22:48:02 -0300

    feat: day11 part1

---
+++ 2024/day11/challenge/pt1.go
@@ -0,0 +1,54 @@
+package challenge
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/wesleyfebarretos/advent-of-code/2024/utils"
+)
+
+func Pt1() {
+	stones := parsePuzzle(utils.GetPuzzle())
+
+	for i := 0; i < 25; i++ {
+		innerStones := []string{}
+
+		for _, stone := range stones {
+			if stone == "0" {
+				innerStones = append(innerStones, "1")
+			} else if (len(stone) & 1) == 0 {
+				halfLen := len(stone) / 2
+
+				innerStones = append(innerStones, stone[0:halfLen])
+
+				innerStones = append(innerStones, removeZeroesFromLeft(stone[halfLen:]))
+
+			} else {
+				stoneAsNum, _ := strconv.Atoi(stone)
+
+				innerStones = append(innerStones, strconv.Itoa(stoneAsNum*2024))
+			}
+		}
+
+		stones = innerStones
+	}
+
+	fmt.Printf("Part 1 -> %d", len(stones))
+}
+
+func removeZeroesFromLeft(num string) string {
+	numWithoutZeroes := strings.TrimLeft(num, "0")
+
+	if numWithoutZeroes == "" {
+		return "0"
+	}
+
+	return numWithoutZeroes
+}
+
+func parsePuzzle(puzzle string) []string {
+	reg := regexp.MustCompile(`\s+`)
+	return strings.Split(reg.ReplaceAllString(puzzle, " "), " ")
+}
---
+++ 2024/day11/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/wesleyfebarretos/advent-of-code/2024/day11/challenge"
+)
+
+func main() {
+	challenge.Pt1()
+	fmt.Println()
+	challenge.Pt2()
+}
